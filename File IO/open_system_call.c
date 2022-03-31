c466dd4aa56cfe198216b2392960178193987027
SEBIN SEBASTIAN <mailmesebin00@gmail.com>
2022-03-31 16:16:49 +0000

    Implement open system call

---
+++ File IO/open_system_call.c
@@ -0,0 +1,28 @@
+//implementing the open() system call in linux programming
+
+//will work without sys/types.h and sys/stat.h
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+//following code opens the file and stores the file descriptor in the variable fd
+int main()
+{
+    int fd;
+//the flags argument maybe bitwise ORed with the following constants: O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_EXCL, O_NOCTTY, O_TRUNC, and O_APPEND.
+    fd = open("/workspaces/Linux-System-Programming/File IO/sample.txt", O_RDONLY | O_CREAT);
+//error checking , implement this also
+    if (fd < 0)
+    {
+        perror(""); //for perror, no need to provide any default messsages, it will print standard error messages
+        exit(1);
+    }
+    else
+    {
+        printf("file opened successfully\n");
+    }
+}
